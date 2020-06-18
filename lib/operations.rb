1cbf339c23e78e6a568f69c8ad6687ceb58b127e
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-18 00:02:04 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
 def unsafe?(speed)
-
+if speed>60
+ true
+  
+elsif speed<40
+ true
+  
+elsif 40<speed
+ false 
+ 
+elsif 60>speed
+end
 end
 
 
 
 def not_safe?(speed)
-	
+		speed ? speed<40 || speed>60: speed<60 || speed>40
 end
 	
 
