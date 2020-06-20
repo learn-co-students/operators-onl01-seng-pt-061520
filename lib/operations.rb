cdb5624e562c68b5de71f12e883c07514f64b0be
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-20 03:11:26 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,27 @@
+#require 'pry'
 def unsafe?(speed)
-
+  if speed >= 60
+    return true
+  end
+  if speed <= 40
+    return true
+  else 
+    return false 
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+
+#60 <=> 40 ? true : false 
+if speed <40 || speed >60 
+  return true 
+else 
+  return false 
+end
+
+	 #binding.pry
 end
 	
-
 
